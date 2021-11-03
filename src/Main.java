72fb10027809e30c7857decd8564f1fa683f261b
Forfetch <mabuttta@gmail.com>
2021-11-03 10:50:47 +0300

    New commit files

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+/*
+* Создаем новый проект Homework5
+* добавляем папку .gitignore и внес в исключения папки
+*  bin/
+* admin/
+* config/
+* .sass
+* в терминале проекта командой git init инициализируем GIT
+* добавляем глобальные настройки пользователя командами:
+* git config --global user.name 'Forfetch'
+* git config --global user.email 'mabuttta@gmail.com'
+* пщскольку папка bin была занесена в игнор добавляем файлы на коммит командой git add .
+*
+*
+*    */
+    }
+}
